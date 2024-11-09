c6149c5e06aeb615b83874b0f0b069b261da7cae
Sikic, Mirko <msikic@fi.uba.ar>
2024-11-08 23:42:12 -0300

    [#ID]1,2 Muchas cosas del home para Leo :)

--- front/app.py
+++ front/app.py
@@ -6,11 +6,16 @@ def main():
 
     @app.route('/')
     def Home():
-        return render_template('home.html')
+        generos = {"Acción","Arte","Autoayuda","Aventuras","Biografías","Cocina","Contemporáneo","Cs. Ficción","Distopía","Divulgativos","Drama","Fantasía","Historia","Infantil","Manuales","Memorias","Paranormal","Poesía","Romance","Salud","Suspenso","Terror"}
+        return render_template('home.html',generos=generos)
 
-    @app.route('/login')
-    def login():
+    @app.route('/Login')
+    def Login():
         return render_template('login.html')
+
+    @app.route('/Home/<genero>')
+    def Genero(genero):
+        return render_template('home.html')
 
     app.run(debug=True)
 
