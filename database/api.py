ad1fc4e3726d99a626ae675b78426b12f25c3a86
Ignacio Ramos <ignacio.nahuel.ramos@gmail.com>
2024-11-24 17:06:49 -0300

    connect db

--- database/api.py
+++ database/api.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 import mysql.connector
 
 app = Flask(__name__)
+CORS(app)  # Esto habilita CORS para todas las rutas
 
 DB_CONFIG = {
     'host': 'localhost',
-    'user': 'test',
+    'user': 'nacho',
     'password': '123',
     'database': 'LibrosEnRed'
 }
@@ -169,4 +172,4 @@ def delete_book(book_id):
     return jsonify({"message": "Book deleted successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
