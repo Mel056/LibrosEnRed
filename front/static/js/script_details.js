9aa4be874bbde070f5fe525c392cd0804ac66703
lialaoss <ljhong@fi.uba.ar>
2024-11-26 17:35:02 -0300

    [#62-66] Fetch and display user locations on map

--- front/static/js/script_details.js
+++ front/static/js/script_details.js
@@ -11,3 +11,25 @@ function initMap(latitude, longitude) {
         title: 'Ubicación'
     });
 }
+
+async function cargarUbicacion() {
+    try {
+        const response = await fetch('http://localhost:5001/users'); 
+        const usuarios = await response.json();
+
+
+        usuarios.forEach(usuario => {
+            const latitude = parseFloat(usuario.latitude);  
+            const longitude = parseFloat(usuario.longitude);  
+
+            
+            initMap(latitude, longitude);
+        });
+    } catch (error) {
+        console.error('Error al cargar los usuarios:', error);
+    }
+}
+
+
+cargarUbicacion();
+
