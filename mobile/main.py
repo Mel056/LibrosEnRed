aa1bd8a9abc36418866d00227cb24756f07e7098
pedro <pripa@fi.uba.ar>
2024-11-27 23:04:04 -0300

    bug fixes

--- mobile/main.py
+++ mobile/main.py
@@ -24,7 +24,7 @@ class LibrosEnRedApp(MDApp):
 
     def build(self):
         sm = ScreenManager()
-        # sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(LoginScreen(name='login'))
         sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(MapScreen(name='map'))
         sm.add_widget(LocationSelectScreen(name='location_select'))
